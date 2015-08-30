d520463d0bff5159647211bcbcf847bb08bac9c4
mingis <mingiss@gmail.com>
2015-08-30 19:56:54 +0300

    Launcher i atskira repozitorija

--- Tev/publ/creg_gamyba/F/git_clone.bat
+++ Tev/publ/creg_gamyba/F/git_clone.bat
@@ -4,4 +4,13 @@ git clone https://github.com/mingiss/source.git
 mkdir \kp
 cd \kp
 git clone https://github.com/mingiss/kp_source.git source
+git clone https://github.com/mingiss/kp_bin.git bin
+cd source
+cd kpstart
+mkdir cert
+cd cert
+mkdir Launcher
+cd Launcher
+:: pwd: tev_mercurial
+git clone https://tev_hg@bitbucket.org/tev_hg/launcher.git src
 
